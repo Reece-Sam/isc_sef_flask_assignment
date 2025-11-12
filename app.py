0a1df1a091e4a87042a5cc2993c94b8fabd172ff
Reece-Sam <bonisamkelekoh@gmail.com>
2025-11-12 10:32:49 +0100

    Chores:Flask assignment completed

--- app.py
+++ app.py
@@ -1,19 +1,60 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Welcome to My First Flask API!"
+books = [
+    {"id":1, "name":"Promise me", "genre":"crime,mystery and suspense thriller"},
+    {"id":2, "name":"The 48 laws of power", "genre":"self-help book"},
+    {"id":3, "name":"The alchemist","genre":"adventure and fantasy"}
+]
 
-@app.route('/api/user')
-def get_user():
-    user = {
-        "id": 1,
-        "name": "Alice",
-        "email": "alice@example.com"
-    }
-    return jsonify(user)
+@app.route("/api/books")
+def get_books():
+    return jsonify (books),200 
 
-if __name__ == '__main__':
+@app.route("/api/books", methods=['POST'])
+def create_books():
+    data = request.get_json()
+    new_id = len(books)+1
+    new_book = {"id":new_id, "name":data["name"],"genre":data["genre"]}
+    books.append(new_book)
+    return jsonify(new_book), 200
+
+@app.route("/api/books/<int:book_id>", methods = ['PATCH'])
+def update_books(books_id):
+    data = request.get_json()
+    for book in books:
+        if book['id']== books_id:
+            book["name"]= data.get("name", book["name"])
+            return jsonify(book), 200
+        return jsonify ({"Error Message": "Book Not Found"}), 404
+    
+@app.route("/api/books/<string:genre>", methods = ['DELETE'])
+def delete_books(genre):
+    for book in books:
+        if book['genre']== genre:
+            books.remove(book)
+            return jsonify({"Message": "Successfully deleted"}), 201
+        return jsonify ({"Message": "unsuccessful"}), 404
+    
+@app.route("/api/books/<int:book_id>")
+def get_books_id(books_id):
+    for book in books:
+        if book['id']== books_id:
+            return jsonify(book), 200
+        return jsonify ({"Message": "Not Found"}), 404
+    
+@app.route("/api/books/<book_name>")
+def get_name(books_name):
+    for book in books:
+        if book["name"] == books_name:
+            return jsonify(book), 200
+        return jsonify ({"Message": "Book Not Found"}), 404
+
+@app.route("/api/info")
+def get_book_info():
+    return jsonify()
+
+#Run the server
+if __name__ == "__main__":
     app.run(debug=True)
